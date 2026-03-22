797e91aecba00e5333777cfe5e5a433aeb682275
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-03-22 08:33:35 +0300

    Initial Commit

---
+++ module3lesson10Homework/module3lesson10HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  module3lesson10HomeworkApp.swift
+//  module3lesson10Homework
+//
+//  Created by Максим Минаков on 22.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct module3lesson10HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
